346a3010c98d281f05d0e1fc1021ae444a80ebac
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2021-09-18 20:50:49 -0300

    💻 Java Basic

---
+++ Projeto1.1/OutroTeste2.java
@@ -0,0 +1,19 @@
+
+/**
+ * Escreva uma descrição da classe OutroTeste2 aqui.
+ * 
+ * @author (Lay-RosaLauren) 
+ * @version (1.1 - 24/07/2021)
+ */
+public class OutroTeste2
+{
+    public static void main (String x[]) {
+        String saida = "";
+        
+        saida = x[0] + " - " + x[1]; 
+        
+        System.out.println(saida);
+    }
+    
+    
+}
---
+++ Projeto1.1/Teste2.java
@@ -0,0 +1,18 @@
+
+/**
+ * Escreva uma descrição da classe Teste2 aqui.
+ * 
+ * @author (Lay-RosaLauren) 
+ * @version (1.0 - 24/07/2021)
+ */
+public class Teste2
+{
+    public static void main (String args[])
+    {
+        // variavel com o texto saida
+        String saida;
+        
+        saida= "Esse é um teste do 2do exemplo";
+        System.out.println(saida);
+    }
+}
